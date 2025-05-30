ed1213f9a6def85de8d5fa8c294c226238379835
AfshinD-W <afshinaa705@gmail.com>
2025-05-30 19:43:10 +0330

    Add DbContext class

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SSToDo.Entity;
+
+namespace SSToDo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        #region Entitys
+
+        public DbSet<User> tblUsers { get; set; }
+        public DbSet<Project> tblProjects { get; set; }
+        public DbSet<ProjectUser> tblProjectUsers { get; set; }
+        public DbSet<TaskItem> tblTaskItems { get; set; }
+
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            #region Keys
+
+            modelBuilder.Entity<ProjectUser>()
+                .HasKey(pu => new { pu.ProjectId, pu.UserId });
+
+            modelBuilder.Entity<ProjectUser>()
+                .HasOne(pu => pu.Project)
+                .WithMany(p => p.ProjectUsers)
+                .HasForeignKey(pu => pu.ProjectId);
+
+            modelBuilder.Entity<ProjectUser>()
+                .HasOne(pu => pu.User)
+                .WithMany(u => u.ProjectUsers)
+                .HasForeignKey(pu => pu.UserId);
+
+            modelBuilder.Entity<Project>()
+                .HasOne(p => p.CreatedBy)
+                .WithMany(u => u.CreatedProjects)
+                .HasForeignKey(p => p.CreatedById)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<TaskItem>()
+                .HasOne(t => t.Project)
+                .WithMany(p => p.Tasks)
+                .HasForeignKey(t => t.ProjectId);
+
+            modelBuilder.Entity<TaskItem>()
+                .HasOne(t => t.AssignedTo)
+                .WithMany(u => u.AssignedTasks)
+                .HasForeignKey(t => t.AssignedToId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            #endregion
+        }
+    }
+}
