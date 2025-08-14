59d10b955780ae04ed6b2d8a68c06dd333432116
AfshinD-W <afshinaa705@gmail.com>
2025-08-14 20:44:18 +0330

    create CreateTodoDtoValidator

---
+++ Models/DtoValidator/CreateTodoTaskDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using SSToDo.Models.Dtos;
+
+namespace SSToDo.Models.DtoValidator
+{
+    public class CreateTodoTaskDtoValidator : AbstractValidator<CreateTodoTaskDto>
+    {
+        public CreateTodoTaskDtoValidator()
+        {
+            RuleFor(t => t.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(100).WithMessage("MaximumLenght for title is 100 character.");
+
+            RuleFor(t => t.Description)
+                .MaximumLength(250).WithMessage("MaximumLenght for descriptiom is 250 character.");
+
+            RuleFor(t => t.DueDate)
+                .GreaterThanOrEqualTo(t => t.StartDate).WithMessage("DueDate must be greater or equal to StartDate");
+        }
+    }
+}
