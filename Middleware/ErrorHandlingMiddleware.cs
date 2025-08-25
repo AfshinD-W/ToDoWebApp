1be74225e87b89d825383190f6e8b84e7f65c5be
AfshinD-W <afshinaa705@gmail.com>
2025-08-25 22:17:53 +0330

    add ErrorHandlingMiddleware

---
+++ Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Text.Json;
+using SSToDo.Utilities;
+
+namespace SSToDo.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var statusCode = HttpStatusCode.InternalServerError;
+            var serviceResponse = new ServiceResponse<string>(exception.InnerException?.Message ?? exception.Message);
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)statusCode;
+
+            var json = JsonSerializer.Serialize(serviceResponse);
+
+            return context.Response.WriteAsync(json);
+        }
+    }
+}
